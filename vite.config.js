e11269732aa2d04ffeea9a819c82bc500b89c761
ibrahima <hafismtechnology@gmail.com>
2024-11-30 13:13:37 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
+        index: path.resolve(__dirname, 'index.html'),
         card: path.resolve(__dirname, 'src/pages/card.html'),
         cardResponsive : path.resolve(__dirname, 'src/pages/masterCard.html'),
         masterCard: path.resolve(__dirname, 'src/pages/masterCard.html'),
