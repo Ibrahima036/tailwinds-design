d9abcf26e4078900f851a86b962c260740d7acb4
ibrahima <hafismtechnology@gmail.com>
2024-11-30 13:27:50 +0000

    deploeyemnt

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
       input: {
         index: path.resolve(__dirname, 'index.html'),
         card: path.resolve(__dirname, 'src/pages/card.html'),
-        cardResponsive : path.resolve(__dirname, 'src/pages/responsive-cardgit a.html'),
+        cardResponsive : path.resolve(__dirname, 'src/pages/responsive-card.html'),
         masterCard: path.resolve(__dirname, 'src/pages/masterCard.html'),
       },
     },
